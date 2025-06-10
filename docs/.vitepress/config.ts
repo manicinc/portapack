549ab25f4bae365139292f5a4f6ef05f730846e6
Victor Evogor <victorevogor0001@gmail.com>
2025-06-10 17:24:12 +0100

    add ga

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,26 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig, HeadConfig } from 'vitepress';
 import { buildDocsSidebar } from './sidebar-generator';
+
+const GA_ID = process.env.GA_ID;
+
+const gaHead: HeadConfig[] = GA_ID
+  ? [
+      [
+        'script',
+        { async: 'true', src: `https://www.googletagmanager.com/gtag/js?id=${GA_ID}` }
+      ],
+      [
+        'script',
+        {},
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_ID}');
+        `
+      ]
+    ]
+  : [];
 
 export default defineConfig({
   base: '/portapack/',
@@ -11,16 +32,17 @@ export default defineConfig({
   head: [
     ['link', { rel: 'icon', href: '/portapack/favicon.ico' }],
     ['meta', { name: 'og:title', content: 'PortaPack' }],
-    [
-      'meta',
-      { name: 'og:description', content: 'Bundle & Minify HTML into a Single Portable File' },
-    ],
-    ['meta', { name: 'og:image', content: '/portapack/portapack.jpg' }], // Updated to use your non-transparent logo
+    ['meta', {
+      name: 'og:description',
+      content: 'Bundle & Minify HTML into a Single Portable File'
+    }],
+    ['meta', { name: 'og:image', content: '/portapack/portapack.jpg' }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ...gaHead
   ],
 
   themeConfig: {
-    logo: '/portapack-transparent.png', // This path is relative to the public directory
+    logo: '/portapack-transparent.png',
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/manicinc/portapack' },
@@ -28,15 +50,15 @@ export default defineConfig({
       { icon: 'discord', link: 'https://discord.gg/DzNgXdYm' },
       {
         icon: {
-          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>',
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>'
         },
-        link: 'https://www.linkedin.com/company/manic-agency-llc/',
-      },
+        link: 'https://www.linkedin.com/company/manic-agency-llc/'
+      }
     ],
 
     footer: {
       message: 'Released under the MIT License',
-      copyright: '© 2025 Manic Agency. All rights reserved.',
+      copyright: '© 2025 Manic Agency. All rights reserved.'
     },
 
     nav: [
@@ -48,12 +70,12 @@ export default defineConfig({
           { text: 'CLI Reference', link: '/cli' },
           { text: 'API', link: '/api/README' },
           { text: 'Configuration', link: '/configuration' },
-          { text: 'Advanced Usage', link: '/advanced' },
-        ],
+          { text: 'Advanced Usage', link: '/advanced' }
+        ]
       },
       { text: 'Contributing', link: '/contributing' },
-      { text: 'Architecture', link: '/architecture'},
-      { text: 'Roadmap', link: 'roadmap'}
+      { text: 'Architecture', link: '/architecture' },
+      { text: 'Roadmap', link: 'roadmap' }
     ],
 
     sidebar: {
@@ -64,9 +86,9 @@ export default defineConfig({
           items: [
             { text: 'Introduction', link: '/getting-started/' },
             { text: 'Installation', link: '/getting-started/installation' },
-            { text: 'Quick Start', link: '/getting-started/quick-start' },
-          ],
-        },
+            { text: 'Quick Start', link: '/getting-started/quick-start' }
+          ]
+        }
       ],
       '/cli/': [
         {
@@ -74,9 +96,9 @@ export default defineConfig({
           items: [
             { text: 'Overview', link: '/cli/' },
             { text: 'Commands', link: '/cli/commands' },
-            { text: 'Options', link: '/cli/options' },
-          ],
-        },
+            { text: 'Options', link: '/cli/options' }
+          ]
+        }
       ],
       '/configuration/': [
         {
@@ -84,10 +106,11 @@ export default defineConfig({
           items: [
             { text: 'Overview', link: '/configuration/' },
             { text: 'Options', link: '/configuration/options' },
-            { text: 'Advanced', link: '/configuration/advanced' },
-          ],
-        },
-      ],
-    },
-  },
+            { text: 'Advanced', link: '/configuration/advanced' }
+          ]
+        }
+      ]
+    }
+  }
 });
+
